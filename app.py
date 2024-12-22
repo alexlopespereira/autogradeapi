09ee6dc444c6f38644fa4ae378907f7760731feb
Alex Lopes Pereira <alexlopespereira@gmail.com>
2024-12-21 21:44:23 -0400

    api functions

--- app.py
+++ app.py
@@ -54,13 +54,15 @@ def prompt_completion(user_prompt):
     response = client.chat.completions.create(
         model=OPENAI_GPT_MODEL,
         messages=[
-            {"role": "user", "content": "You are a helpful assistant that generates Python functions."},
             {"role": "user",
              "content": f"In your answer return only the python code, and no text before neither after the code. Write a Python function for the following prompt:\n{user_prompt}"}
         ],
-        max_completion_tokens=150
+        max_completion_tokens=1500
     )
     generated_code = response.choices[0].message.content.strip().replace("```","")
+    if generated_code == "":
+        print("empty code")
+        raise Exception("The generated code is empty. You probably sent a too large prompt")
     generated_code = re.sub(r"^python\s*", "", generated_code)
     print(generated_code)
     return generated_code
@@ -216,7 +218,7 @@ async def validate_student_code():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-#     prompt_completion("""Escreva uma função Python para gerar um dicionário que contenha pares (chave e valor) de números. A chave deve variar de 1 a n, e os valores sejam o valor da chave elevado ao quadrado.
-# Exemplo: Para n = 5
-# Resultado Esperado: {"1": 1, "2": 4, "3": 9, "4": 16, "5": 25}""")
+    # app.run(debug=True)
+    prompt_completion("""Escreva uma função Python para gerar um dicionário que contenha pares (chave e valor) de números. A chave deve variar de 1 a n, e os valores sejam o valor da chave elevado ao quadrado.
+Exemplo: Para n = 5
+Resultado Esperado: {"1": 1, "2": 4, "3": 9, "4": 16, "5": 25}""")
